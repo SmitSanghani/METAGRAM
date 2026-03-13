c730129972f386216d66710f271a637d95cbac37
smits.goldenmace <smits.goldenmace@gmail.com>
2026-03-14 00:20:06 +0530

    feat: implement user authentication, registration, login, logout, and profile management functionalities.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { getDataUri } from "../utils/dataUri.js";
+import { getDataUri } from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post.model.js";
 import { Reel } from "../models/reel.model.js";
